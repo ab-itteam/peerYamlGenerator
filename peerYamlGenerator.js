fa20a57f139b324d349ad4a3efd0658134a17b0f
nick <nick@ab-it.io>
2021-05-21 17:42:12 +0300

    add docker ca file

--- peerYamlGenerator.js
+++ peerYamlGenerator.js
@@ -3,14 +3,39 @@ const yargs = require('yargs');
 
 const fs   = require('fs');
 const argv = yargs.argv
-let doc = yaml.load(fs.readFileSync('variable-crypto.yaml', 'utf8'));
 
-doc.PeerOrgs[0].Name = argv.u;
-doc.PeerOrgs[0].Domain = argv.d;
-doc.PeerOrgs[0].Users.Count =  parseInt(argv.c);
-console.log(doc);
+
+const name = argv.u;
+const domain = argv.d;
+const userCount = parseInt(argv.c);
+
+
+
+
+//the crypto Yaml file
+let doc = yaml.load(fs.readFileSync('variable-crypto.yaml', 'utf8'));
+doc.PeerOrgs[0].Name = name;
+doc.PeerOrgs[0].Domain = domain;
+doc.PeerOrgs[0].Users.Count =  userCount;
 fs.writeFile('./newPeer.yaml', yaml.dump(doc), (err) => {
     if (err) {
         console.log(err);
     }
 });
+
+//the docker CA yaml file
+let docCa = yaml.load(fs.readFileSync('docker-compose-ca-variable.yaml', 'utf8'));
+
+docCa.services[`ca_${name}`] = docCa.services[`ca_variable`];
+docCa.services[`ca_${name}`].container_name = `ca_${name}`;
+console.log(docCa.services[`ca_${name}`].environment)
+docCa.services[`ca_${name}`].environment.push(`FABRIC_CA_SERVER_CA_NAME=ca_${name}`);
+delete docCa.services[`ca_variable`];
+
+fs.writeFile('./docker-compose-ca-new.yaml', yaml.dump(docCa), (err) => {
+    if (err) {
+        console.log(err);
+    }
+});
+
+
