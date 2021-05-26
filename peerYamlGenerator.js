b270602d6294716be0f3da90003b28e123ccae70
nick <nick@ab-it.io>
2021-05-26 17:43:46 +0300

    bring structure in project

--- peerYamlGenerator.js
+++ peerYamlGenerator.js
@@ -1,14 +1,23 @@
 const yaml = require('js-yaml');
-
 const yargs = require('yargs');
-
 const fs   = require('fs');
+
+//argument parsing
 const argv = yargs.argv
-
-
 const name = argv.u;
 const domain = argv.d;
 const userCount = parseInt(argv.c);
+
+
+//read templates from current location
+const location = `${__dirname}/templates`;
+//output to working directory
+const output = `${process.cwd()}/generated`;
+
+
+
+
+console.log(location)
 
 console.log(domain);
 console.log(name);
@@ -16,18 +25,13 @@ console.log(name);
 
 
 //the crypto Yaml file
-let doc = yaml.load(fs.readFileSync('variable-crypto.yaml', 'utf8'));
+let doc = yaml.load(fs.readFileSync(`${location}/variable-crypto.yaml`, 'utf8'));
 doc.PeerOrgs[0].Name = name;
 doc.PeerOrgs[0].Domain = domain;
 doc.PeerOrgs[0].Users.Count =  userCount;
-fs.writeFile('./newPeer.yaml', yaml.dump(doc), (err) => {
-    if (err) {
-        console.log(err);
-    }
-});
 
 //the docker CA yaml file
-let docCa = yaml.load(fs.readFileSync('docker-compose-ca-variable.yaml', 'utf8'));
+let docCa = yaml.load(fs.readFileSync(`${location}/docker-compose-ca-variable.yaml`, 'utf8'));
 
 docCa.services[`ca_${name}`] = docCa.services[`ca_variable`];
 docCa.services[`ca_${name}`].container_name = `ca_${name}`;
@@ -35,14 +39,10 @@ console.log(docCa.services[`ca_${name}`].environment)
 docCa.services[`ca_${name}`].environment.push(`FABRIC_CA_SERVER_CA_NAME=ca_${name}`);
 delete docCa.services[`ca_variable`];
 
-fs.writeFile('./docker-compose-ca-new.yaml', yaml.dump(docCa), (err) => {
-    if (err) {
-        console.log(err);
-    }
-});
+
 
 //the docker compose peer file
-let docPeer = yaml.load(fs.readFileSync('docker-compose-variable.yaml', 'utf8'));
+let docPeer = yaml.load(fs.readFileSync(`${location}/docker-compose-variable.yaml`, 'utf8'));
 
 const fqdn = `peer0.${name}.${domain}`;
 docPeer.volumes[fqdn] = {external: false};
@@ -68,10 +68,18 @@ docPeer.services[fqdn].volumes.push(`${fqdn}:/var/hyperledger/production`);
 
 delete docPeer.services[`peer0.variable`];
 
-fs.writeFile('./docker-compose-orgpeer-new.yaml', yaml.dump(docPeer), (err) => {
-    if (err) {
-        console.log(err);
-    }
-});
+
+//no adaptations needed in couchdb
+let couchdb = yaml.load(fs.readFileSync(`${location}/docker-compose-couch-variable.yaml`, 'utf8'));
 
 
+//make sure the output dir exists
+fs.mkdirSync(`${output}/docker`, { recursive: true });
+
+//write everything to files.
+Promise.all([
+fs.writeFileSync(`${output}/docker/docker-compose-couch-new.yaml`, yaml.dump(couchdb)),
+fs.writeFileSync(`${output}/docker/docker-compose-orgpeer-new.yaml`, yaml.dump(docPeer)),
+fs.writeFileSync(`${output}/docker/docker-compose-ca-new.yaml`, yaml.dump(docCa)),
+fs.writeFileSync(`${output}/newPeer.yaml`, yaml.dump(doc))
+]).then(() => console.log("done")).catch((e) => console.error(e));
